89c2edd4e0d8bad9c432a38bd19afbd04a5e5919
Mauricio G. Coello <mgonco95@Mauricios-Alienware.local>
2017-03-08 01:44:04 -0600

    Initial Commit

---
+++ Observer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Observer
+//
+//  Created by Mauricio G. Coello on 3/8/17.
+//  Copyright © 2017 Mauricio G. Coello. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
